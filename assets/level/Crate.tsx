e4a787f3eff31abe3ca06e02fa1dc51b351f2f35
Tobias Mogdans <tobias.mogdans@mayflower.de>
2022-12-23 16:04:59 +0100

    tiled level

---
+++ assets/level/Crate.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Crate" tilewidth="58" tileheight="42" tilecount="1" columns="1">
+ <image source="../crate.png" width="58" height="42"/>
+</tileset>
